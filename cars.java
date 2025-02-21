b90e8d55f64d7282ccef7e35b469099981c896fc
fazilmca <fazilmca1@gmail.com>
2025-02-21 16:38:58 +0530

    Create cars.java

---
+++ cars.java
@@ -0,0 +1,28 @@
+package MODULE1;
+
+public class cars {
+
+	public cars() {
+		
+	}
+
+	public static void main(String[] args) {
+		PRODUCT p1 =new PRODUCT();
+		p1.pcode= "Car123";
+		p1.pname = "Benz";
+		p1.price =10000;
+		System.out.println("Displaying p1:");
+		p1.display();
+		
+		PRODUCT p2 = new PRODUCT ("Jaguar","Car426",25000);
+		System.out.println("Displaying p2:");
+		p2.display();
+		
+		PRODUCT p3 = new PRODUCT ("Maruthi","Car600",50000);
+		System.out.println("Displaying p3:");
+		p3.display();
+		
+		PRODUCT p = p3.getPrice()<(p1.price<p2.price?p1.price:p2.price)?p3:(p1.price<p2.price?p1:p2);
+		p.display();
+	}
+}
