077215a46a75798b9e3e8a9c78ece9541399ec5f
Muhammed Fazil C <fazilmca1@gmail.com>
2025-03-21 14:30:58 +0530

    Create OverLoad.java

---
+++ OverLoad.java
@@ -0,0 +1,31 @@
+package MODULE2;
+import java.util.Scanner;
+class OverLoadDemo {
+	void area(float x) {
+		System.out.println("The area of the square is "+Math.pow(x, 2)+" sq units");
+	}
+	void area(float x, float y) {
+		System.out.println("The area of the rectangle is "+x*y+" sq units");
+	}
+	void area(double x) {
+		double z = 3.14 * x * x;
+		System.out.println("The area of the cirecl is "+z+" sq units");
+	}
+}
+class OverLoad {
+	public static void main (String args[]) {
+		Scanner sc = new Scanner(System.in);
+		OverLoadDemo ob = new OverLoadDemo();
+		System.out.println("Enter side Length of Square:");
+		float squareSide = sc.nextFloat();
+        ob.area(squareSide);
+		System.out.println("Enter length of rectangle:");
+		float rectLength = sc.nextFloat();
+		System.out.print("Enter width of rectangle: ");
+        float rectWidth = sc.nextFloat();
+        ob.area(rectLength, rectWidth);
+		System.out.println("Enter radius of circle: ");
+		double circleRadius = sc.nextDouble();
+        ob.area(circleRadius);
+	}
+}
